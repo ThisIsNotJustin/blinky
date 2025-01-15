8f2d276aa845ceb5962f6413ab2b192a5d2bce10
ThisIsNotJustin <justincolemanonetwo@gmail.com>
2025-01-15 02:56:36 -0600

    set pin to IN before exiting program

--- blink.c
+++ blink.c
@@ -2,6 +2,7 @@
 #include <pigpio.h>
 #include <unistd.h>
 
+// utilizing GPIO pin 26
 #define LED_PIN 26
 
 int main() {
@@ -10,12 +11,14 @@ int main() {
         printf("piGPIO init failed\n");
         return 1;
     }
-
+    
+    // open to out
     if (gpioSetMode(LED_PIN, PI_OUTPUT)) {
-        printf("piGPIO set failed");
+        printf("piGPIO set to out failed\n");
         return 1; 
     }
 
+    // set to 1 (on) for 1 second, off (0) for 1 second, repeat...
     for (int i = 0; i < 10; i++) {
         gpioWrite(LED_PIN, 1);
         gpioSleep(PI_TIME_RELATIVE, 1, 0);
@@ -23,6 +26,10 @@ int main() {
         gpioSleep(PI_TIME_RELATIVE, 1, 0);
     }
 
+    // reinstate to in
+    if (gpioSetMode(LED_PIN, PI_INPUT)) {
+        printf("piGPIO set to in failed\n");
+    }
     gpioTerminate();
 
     return 0;
